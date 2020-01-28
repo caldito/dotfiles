97d54f7090130617a60e3b289036eb16e0fad944
caldito <100383255@alumnos.uc3m.es>
2020-01-28 12:43:46 +0100

    Remove unnecessary files.

--- arch.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-yaourt -S arc-gtk-theme papirus-icon-theme vlc tlp git code jdk8 spotify insync intellij-idea-community-edition pycharm-community-edition virtualbox skypeforlinux-stable-bin gitkraken libreoffice-fresh neofetch rememberthemilk tusk minetime whatsapp-web-desktop chrome-gnome-shell ruby-rdoc ruby-fusuma insync-nautilus
-
